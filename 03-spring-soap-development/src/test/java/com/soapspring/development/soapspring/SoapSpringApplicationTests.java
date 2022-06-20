6a385f7e4a981471074f5b10a28d5bccdf5d3d25
hs-mxmx <d.perezmoreno@outlook.es>
2022-06-20 19:57:55 +0200

    Spring SOAP development

---
+++ 03-spring-soap-development/src/test/java/com/soapspring/development/soapspring/SoapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soapspring.development.soapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
